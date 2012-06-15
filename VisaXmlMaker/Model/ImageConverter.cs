05e576854f56f4b9412036cb7981c891ba5d2c1d
Andrei Lemzikov <andylem@gmail.com>
2012-06-15 11:51:18 +0300

    ImageConverter Implemented
    http://www.dailycoding.com/Posts/convert_image_to_base64_string_and_base64_string_to_image.aspx

--- VisaXmlMaker/Model/ImageConverter.cs
+++ VisaXmlMaker/Model/ImageConverter.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace VisaXmlMaker.Model
 {
-    class ImageConverter
+    public abstract class ImageConverter
     {
+        public static string ConvertImageToBase64(Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+        public static Image ConvertBase64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
     }
 }
